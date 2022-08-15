beb0c06469ec68ab2b299e2011160e5af6707923
Marin <Marin@PC>
2022-08-15 08:52:20 -0700

    end vector

--- backend/modules/hello_world.py
+++ backend/modules/hello_world.py
@@ -5,3 +5,10 @@ hello_world_bp = Blueprint('hello_world', __name__)
 @hello_world_bp.route('/')
 def hello_world():
     return "<h1>Hello, World! dfasrf<h1>"
+
+
+@hello_world_bp.route('/hello')
+def hello():
+    return {
+        "text":"hello"
+    }
--- frontend/app.js
+++ frontend/app.js
@@ -2,23 +2,11 @@ let elem = document.querySelector('.row');
 
 
 $.ajax({
-    url: 'http://localhost:5000/article/favorite',
+    url: 'http://localhost:5000/hello',
     method: 'GET',
     success: function(response){
       response.forEach(element => {
         console.log(element)
-        let block = ''
-        block += '\
-        <div class="col">\
-        <div class="card" style="width: 18rem;">\
-        <div class="card-body">\
-          <h5 class="card-title">'+element['title']+'</h5>\
-          <p class="card-text">'+element['body']+'</p>\
-        </div>\
-      </div>\
-      </div>\
-       ';
-        elem.innerHTML +=block;
       });
    }
   })
